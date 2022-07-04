2e7f9f392ceb5cb6ae6ad710d8956c9bf90567dc
github-action-benchmark <github@users.noreply.github.com>
2022-07-04 07:17:47 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for e4267add86d6b51ab6f95c68a836712e59e05b17

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1656918897775,
+  "lastUpdate": 1656919067073,
   "repoUrl": "https://github.com/hamzeh-khanpour/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -4824,6 +4824,44 @@ window.BENCHMARK_DATA = {
             "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
             "value": 6.168899249718254,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "58598278+hamzeh-khanpour@users.noreply.github.com",
+            "name": "Hamzeh Khanpour",
+            "username": "hamzeh-khanpour"
+          },
+          "committer": {
+            "email": "58598278+hamzeh-khanpour@users.noreply.github.com",
+            "name": "Hamzeh Khanpour",
+            "username": "hamzeh-khanpour"
+          },
+          "distinct": true,
+          "id": "e4267add86d6b51ab6f95c68a836712e59e05b17",
+          "message": "Update analysis_AFB_stage1.py",
+          "timestamp": "2022-07-04T11:24:28+04:30",
+          "tree_id": "1d8f8c137df949b87e6d73c6334c2b04363069fa",
+          "url": "https://github.com/hamzeh-khanpour/FCCAnalyses/commit/e4267add86d6b51ab6f95c68a836712e59e05b17"
+        },
+        "date": 1656919065044,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 14.461251020431519,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 15.394104719161987,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
           }
         ]
       }
