c5d1928110cd2251498896ef0b8ed23c503ab2bb
github-action-benchmark <github@users.noreply.github.com>
2022-07-23 08:39:55 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for c75cacc633eea95d37f690161f4f29d6ab9aa16c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1658565594223,
+  "lastUpdate": 1658565595720,
   "repoUrl": "https://github.com/hamzeh-khanpour/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -6374,6 +6374,40 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "58598278+hamzeh-khanpour@users.noreply.github.com",
+            "name": "Hamzeh Khanpour",
+            "username": "hamzeh-khanpour"
+          },
+          "committer": {
+            "email": "58598278+hamzeh-khanpour@users.noreply.github.com",
+            "name": "Hamzeh Khanpour",
+            "username": "hamzeh-khanpour"
+          },
+          "distinct": true,
+          "id": "c75cacc633eea95d37f690161f4f29d6ab9aa16c",
+          "message": "get_flavour_noabsvalue",
+          "timestamp": "2022-07-23T12:37:21+04:30",
+          "tree_id": "50cc703b93853e1f18060ee42982dc6fd02655ab",
+          "url": "https://github.com/hamzeh-khanpour/FCCAnalyses/commit/c75cacc633eea95d37f690161f4f29d6ab9aa16c"
+        },
+        "date": 1658565595212,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 6.597029897069885,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 4.9434689180410105,
+            "unit": "Evt/s"
           }
         ]
       }
