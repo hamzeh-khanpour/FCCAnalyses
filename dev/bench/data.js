d4743ca093415b7d12c96753b338d2d75b90f488
github-action-benchmark <github@users.noreply.github.com>
2022-09-14 20:29:45 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 7f6693e0287fb647c52950e58536508545df5042

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663187383485,
+  "lastUpdate": 1663187384917,
   "repoUrl": "https://github.com/hamzeh-khanpour/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -7605,6 +7605,45 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "58598278+hamzeh-khanpour@users.noreply.github.com",
+            "name": "Hamzeh Khanpour",
+            "username": "hamzeh-khanpour"
+          },
+          "committer": {
+            "email": "58598278+hamzeh-khanpour@users.noreply.github.com",
+            "name": "Hamzeh Khanpour",
+            "username": "hamzeh-khanpour"
+          },
+          "distinct": true,
+          "id": "7f6693e0287fb647c52950e58536508545df5042",
+          "message": "Update analysis_AFB_stage1_10.py",
+          "timestamp": "2022-09-15T00:50:27+04:30",
+          "tree_id": "f7bfe62fd166cd7fadb6cf823915262fb42e8542",
+          "url": "https://github.com/hamzeh-khanpour/FCCAnalyses/commit/7f6693e0287fb647c52950e58536508545df5042"
+        },
+        "date": 1663187384475,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 8.78462817410181,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 6.636236175608916,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test",
+            "value": 9.120554378179405,
+            "unit": "Evt/s"
           }
         ]
       }
